218762abe107ae76079d20f8f137bbd9132628c9
Maxai <maxali946@gmail.com>
2018-03-11 20:08:33 +0400

    Add files via upload
    80%

--- TextRedactor/MainWindow.xaml.cs
+++ TextRedactor/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,117 @@ namespace TextRedactor
     /// </summary>
     public partial class MainWindow : Window
     {
+        StringBuilder temp;
+
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
+        SaveFileDialog saveFileDialog;
+        OpenFileDialog openFileDialog;
+
+        bool firstSave = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            temp = new StringBuilder();
+
+            saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt";
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 10);
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            File.WriteAllText(tbPath.Text, tbUserText.Text);
+        }
+
+        private void WrapPanel_Click(object sender, RoutedEventArgs e)
+        {
+            var controll = e.Source;
+
+            if (controll as CheckBox == cbAutoSave)
+            {
+                if (firstSave && cbAutoSave.IsChecked == true)
+                {
+                    dispatcherTimer.Start();
+                }
+                else
+                {
+                    cbAutoSave.IsChecked = false;
+                    dispatcherTimer.Stop();
+                }
+
+                return;
+            }
+
+            controll = e.Source as Button;
+
+            if (controll == bOpen)
+            {
+                if (tbUserText.Text != "")
+                {
+                    if (saveFileDialog.ShowDialog() == true)
+                    {
+                        File.WriteAllText(saveFileDialog.FileName, tbUserText.Text);
+                    }
+                }
+
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    tbUserText.Text = File.ReadAllText(openFileDialog.FileName);
+
+                    ToolTip toolTip = new ToolTip();
+
+                    toolTip.Content = tbPath.Text = openFileDialog.FileName;
+
+                    tbPath.ToolTip = toolTip;
+
+                    firstSave = false;
+                }
+            }
+            else
+            if (controll == bSaveAs)
+            {
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    File.WriteAllText(saveFileDialog.FileName, tbUserText.Text);
+
+                    tbPath.Text = saveFileDialog.FileName;
+
+                    firstSave = true;
+                }
+            }
+            else
+            if (controll == bCopy)
+            {
+                temp.Clear();
+                temp.Append(tbUserText.SelectedText);
+            }
+            else
+            if (controll == bPaste)
+            {
+                tbUserText.Text = tbUserText.Text.Insert(tbUserText.SelectionStart, temp.ToString());
+            }
+            else
+            if (controll == bCut)
+            {
+                temp.Clear();
+                temp.Append(tbUserText.SelectedText);
+
+                tbUserText.Text = tbUserText.Text.Remove(tbUserText.SelectionStart, tbUserText.SelectionLength);
+            }
+            else
+            if (controll == bSelectAll) 
+            {
+                tbUserText.Focus();
+                tbUserText.SelectAll();
+            }
         }
     }
 }
